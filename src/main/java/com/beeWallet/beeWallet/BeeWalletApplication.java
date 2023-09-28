fda0393b981d0792b38020e6d93c94ee5f3eeeb7
piotrdeptala <deptala.piotr@gmail.com>
2023-09-28 10:11:25 +0200

    Create the project

---
+++ src/main/java/com/beeWallet/beeWallet/BeeWalletApplication.java
@@ -0,0 +1,13 @@
+package com.beeWallet.beeWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beeWallet/beeWallet/BeeWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beeWallet.beeWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
